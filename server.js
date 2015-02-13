6039578522b38208c53ddd97bca8e1cd3140d8a1
Chris Netz <cnetz89@gmail.com>
2015-02-12 23:46:33 -0700

    custy, order, and products post :smile:

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var Express = require ('express');
 var BodyParser = require ('body-parser');
 var Mongoose = require ('mongoose');
 var Customer = require('./lib/models/customersModel');
+var Product = require('./lib/models/productsModel');
+var Order = require('./lib/models/ordersModel');
 
 var port = 8011;
 
@@ -24,7 +26,30 @@ app.post('/api/customer', function(req, res){
 			res.status(418).json(err);
 		}
 	})
-})
+});
+
+app.post('/api/product', function(req, res){
+	var product = new Product(req.body);
+	product.save(function(err, responce){
+		if(!err){
+			res.status(200).json(product);
+		} else {
+			res.status(418).json(err);
+		}
+	})
+});
+
+app.post('/api/order', function (req, res) {
+	var order = new Order(req.body);
+	order.save(function(err, responce){
+		if(!err){
+			res.status(200).json(order);
+		} else {
+			res.status(418).json(err);
+		}
+	})
+});
+
 
 app.listen(port, function(){
 	console.log('now listening on port: ' + port);
